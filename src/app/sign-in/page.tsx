5b355ed6a5b51578a6bc4c8ed049682c13fbeaac
Anderson Leonardo <anderson.scastilho@hotmail.com>
2023-08-07 14:25:52 -0300

    feat: add first page login

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,7 +1,40 @@
 export default function SignInPage() {
   return (
-    <div className="bg-principal min-h-screen min-w-full">
-      <div>OLA</div>
+    <div className="bg-black min-h-screen min-w-full flex justify-center items-center ">
+      <form className="bg-secundaria border border-color-principal rounded-7 w-28rem h-30rem  flex items-center flex-col justify-evenly">
+        <div>
+          <h1 className="text-[2.0rem]">Sign In</h1>
+          <p className="text-[0.87rem]">
+            Digite os seus dados de acesso no campo abaixo
+          </p>
+        </div>
+
+        <div className="flex items-center, justify-center gap-6 flex-col ">
+          <div className="flex flex-col w-80 gap-1">
+            <label htmlFor="">Email:</label>
+            <input
+              type="text"
+              className="rounded-7 p-1 text-texto-principal outline-0 h-10"
+            />
+          </div>
+
+          <div className="flex flex-col w-80 gap-1">
+            <label htmlFor="">Senha:</label>
+            <input
+              type="password"
+              className="rounded-7 p-1 text-texto-principal outline-0 h-10"
+            />
+          </div>
+
+          <p className="text-[0.9rem] underline hover:cursor-pointer">
+            Esqueci minha senha
+          </p>
+        </div>
+
+        <button className="border border-7 rounded-7 p-2 w-60 bg-principal inset-1 active:shadow-entry active:text-[0.9rem]">
+          Entrar
+        </button>
+      </form>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,39 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        'principal': "#3498db",
-        "secundaria": "#2ecc71",
-        "destaque": "#f39c12",
-        "fundo": "#f5f5f5",
+        "principal": "#8b0000",
+        "secundaria": "#a1030b",
+        "destaque": "#663366",
+        "fundo": "#d30519",
         "texto-principal": "#000000",
-        "texto-secundario": "#333333"
+        "texto-secundario": "#bdc3c7",
+      },
+      borderColor: {
+        "color-principal": "#000000"
+      },
+      borderRadius: {
+        "7": "7px",
+      },
+      width: {
+        "28rem": "28rem"
+      },
+      height: {
+        "30rem": "30rem"
+      },
+      boxShadow: {
+        'entry': 'inset -4px 4px 0 #222 '
       }
     },
   },
