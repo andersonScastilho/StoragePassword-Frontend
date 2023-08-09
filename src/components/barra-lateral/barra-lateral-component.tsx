c2460d3289ace0951f12d62d4f68fc947d46cb50
Anderson Leonardo <anderson.scastilho@hotmail.com>
2023-08-09 16:13:45 -0300

    add icon to side bara

--- src/components/barra-lateral/barra-lateral-component.tsx
+++ src/components/barra-lateral/barra-lateral-component.tsx
@@ -1,5 +1,6 @@
 import { MdOutlineStorage } from "react-icons/md";
 import { FiUser } from "react-icons/fi";
+import { AiOutlinePlus } from "react-icons/ai";
 import Link from "next/link";
 
 export const SideBarComponent = () => {
@@ -8,6 +9,7 @@ export const SideBarComponent = () => {
       <Link href="/storage">
         <MdOutlineStorage size={30} cursor="pointer" />
       </Link>
+      <AiOutlinePlus size={30} cursor="pointer" />
       <FiUser size={30} cursor="pointer" />
     </section>
   );
