5942b5da6b520134ea21be45bc36d4e8223605e4
Anderson Leonardo <anderson.scastilho@hotmail.com>
2023-08-15 13:48:57 -0300

    corrigindo private routes

--- src/guard/authentication.guard.tsx
+++ src/guard/authentication.guard.tsx
@@ -1,30 +1,40 @@
+"use client";
 import { useAppSelector } from "@/hooks/redux.hooks";
 import { useRouter } from "next/navigation";
 import { ReactNode, useEffect } from "react";
 import { APP_ROUTES } from "@/constants/app-routes";
 import { useDispatch } from "react-redux";
 import { userRefreshToken } from "@/store/toolkit/user/user.slice";
+import { findCookie } from "@/functions/cookies";
 
 type PrivateRouteProps = {
-  children: ReactNode
-}
+  children: ReactNode;
+};
 
-export const AuthenticationGuard = ({children}:PrivateRouteProps) => {
-  const {push} = useRouter()
-  const dispatch = useDispatch()
-  
-  const {isAuthenticated}= useAppSelector((state) => state.userReducer)
+export const AuthenticationGuard = ({ children }: PrivateRouteProps) => {
+  const { push } = useRouter();
+  const dispatch = useDispatch();
 
-useEffect(() => {
-  if(!isAuthenticated){
-    push(APP_ROUTES.public.signIn)
-  }
-},[])
+  const { isAuthenticated } = useAppSelector((state) => state.userReducer);
+
+  useEffect(() => {
+    async function refreshTokenAndCheckAuth() {
+      if (!isAuthenticated) {
+        const dispatchReturn = await dispatch(userRefreshToken() as any);
+
+        if (dispatchReturn.payload.isAuthenticated !== true) {
+          push(APP_ROUTES.public.signIn);
+        }
+      }
+    }
+
+    refreshTokenAndCheckAuth();
+  }, [isAuthenticated, dispatch, push]);
 
   return (
     <>
-    {!isAuthenticated && null}
-    {isAuthenticated && children}
+      {!isAuthenticated && null}
+      {isAuthenticated && children}
     </>
-  )
+  );
 };
