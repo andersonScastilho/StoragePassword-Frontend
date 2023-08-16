e14b1fbbb62334dd43dd4483404d3fb45924322c
Anderson Leonardo <anderson.scastilho@hotmail.com>
2023-08-15 21:45:45 -0300

    feat: change method to navigate routes

--- src/components/barra-lateral/barra-lateral-component.tsx
+++ src/components/barra-lateral/barra-lateral-component.tsx
@@ -1,14 +1,15 @@
+'use client'
 import { MdOutlineStorage } from "react-icons/md";
 import { FiUser } from "react-icons/fi";
 import { AiOutlinePlus } from "react-icons/ai";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export const SideBarComponent = () => {
+  const {push} = useRouter()
+  
   return (
     <section className="w-20 h-full bg-fundo-secundario border border-none rounded-e-md p-5 flex flex-col gap-10">
-      <Link href="/storage">
-        <MdOutlineStorage size={30} cursor="pointer" />
-      </Link>
+        <MdOutlineStorage  onClick={() => push('/storage')} size={30} cursor="pointer" />
       <AiOutlinePlus size={30} cursor="pointer" />
       <FiUser size={30} cursor="pointer" />
     </section>
