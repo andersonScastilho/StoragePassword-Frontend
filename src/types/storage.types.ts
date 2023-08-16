57ee124a9f5bb466450a14ce1d36aabf0646c807
Anderson Leonardo <anderson.scastilho@hotmail.com>
2023-08-15 22:51:28 -0300

    feat: add type to storage

---
+++ src/types/storage.types.ts
@@ -0,0 +1,11 @@
+export interface Storage {
+  props: {
+    password: string;
+    account: string;
+    usageLocation: string;
+    description: string;
+    link: string;
+    userId: string;
+    storageId: string;
+  };
+}
