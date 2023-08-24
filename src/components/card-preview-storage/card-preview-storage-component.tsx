78279a141a294cb27d8101f4c7dce961db685121
Anderson Leonardo <anderson.scastilho@hotmail.com>
2023-08-23 22:15:23 -0300

    Alterando card

--- src/components/card-preview-storage/card-preview-storage-component.tsx
+++ src/components/card-preview-storage/card-preview-storage-component.tsx
@@ -2,19 +2,30 @@ import { Storage } from "@/types/storage.types";
 import CustomButton from "../custom-button/custom-button-comonent";
 import CustomInput from "../custom-input/custom-input-component";
 import { CustomLabelCompoent } from "../custom-label/custom-label-component";
+import { MdOutlineEmail } from "react-icons/md";
+import { AiOutlineLink } from "react-icons/ai";
 
 export const CardStorageComponent = ({ props }: Storage) => {
   return (
-    <div className="w-80 h-52 p-3 border bg-color-frescor rounded-md flex flex-col gap-5 hover:cursor-pointer hover:scale-105">
-      <h1 className="text-center">{props.usageLocation}</h1>
-      <div>
-        <CustomLabelCompoent>Username/email:</CustomLabelCompoent>
-        <CustomInput readOnly value={props.account} />
+    <div className="w-96 h-48 p-2 border gap-3 bg-cyan-600 rounded-md flex flex-col hover:scale-105">
+      <h1 className="text-center font-semibold text-[1.1rem]">
+        {props.usageLocation}
+      </h1>
+      <div className="flex flex-col gap-4">
+        <p className="flex gap-3 items-center bg-white p-1 border rounded-md text-[0.9rem]">
+          <MdOutlineEmail size={20} />
+          {props.account}
+        </p>
+        <a
+          href={`${props.link}`}
+          target="_blank"
+          className="flex gap-3 items-center p-1 bg-white border rounded-md text-[0.9rem]"
+        >
+          <AiOutlineLink size={20} />
+          {props.link}
+        </a>
       </div>
-      <div className="grid gap-3 grid-cols-2">
-        <CustomButton>Editar</CustomButton>
-        <CustomButton>Excluir</CustomButton>
-      </div>
+      <CustomButton>Abrir</CustomButton>
     </div>
   );
 };
