4369eff705c34390100f6af8f4dc1669675c4e7e
Anderson Leonardo <anderson.scastilho@hotmail.com>
2023-08-31 17:43:01 -0300

    Feat: add chakra ui

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,9 +1,17 @@
 // app/providers.tsx
 "use client";
 
+import { CacheProvider } from "@chakra-ui/next-js";
+import { ChakraProvider } from "@chakra-ui/react";
 import { Provider } from "react-redux";
 import { store } from "../store/store";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <CacheProvider>
+        <ChakraProvider>{children}</ChakraProvider>
+      </CacheProvider>
+    </Provider>
+  );
 }
