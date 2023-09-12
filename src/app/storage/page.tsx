308c8e1475472be80a1979b9d7acc9136ffeec40
Anderson Leonardo <anderson.scastilho@hotmail.com>
2023-09-12 16:10:19 -0300

    feat: add loading to fetch storage

--- src/app/storage/page.tsx
+++ src/app/storage/page.tsx
@@ -1,11 +1,14 @@
 "use client";
-import { SideBarComponent } from "@/components/barra-lateral/barra-lateral-component";
 import { HeaderComponent } from "@/components/header/header-component";
+import LoadingComponent from "@/components/loading/loading-component";
 import { StorageCardsComponent } from "@/components/storage-cards/storage-cards-component";
+import { useAppSelector } from "@/hooks/redux.hooks";
 
 export default function Storage() {
+  const { isLoading } = useAppSelector((state) => state.storageReducer);
   return (
     <main className="min-h-screen min-w-full bg-primary flex flex-col gap-1">
+      {isLoading && <LoadingComponent />}
       <HeaderComponent />
       <section className="flex-grow flex gap-4 p-5">
         <StorageCardsComponent />
