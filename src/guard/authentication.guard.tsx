32bd21bf58c379489a5b64d9abedf0f29b15d271
Anderson Leonardo <anderson.scastilho@hotmail.com>
2023-09-12 20:45:08 -0300

    feat: add loading

--- src/guard/authentication.guard.tsx
+++ src/guard/authentication.guard.tsx
@@ -9,6 +9,7 @@ import {
   userRefreshToken,
 } from "@/store/toolkit/Auth/auth.slice";
 import { checkIsAuthenticated } from "@/functions/check-is-authenticated";
+import LoadingComponent from "@/components/loading/loading-component";
 
 type PrivateRouteProps = {
   children: ReactNode;
@@ -17,23 +18,29 @@ type PrivateRouteProps = {
 export const AuthenticationGuard = ({ children }: PrivateRouteProps) => {
   const { push } = useRouter();
   const dispatch = useDispatch();
+  const [isLoading, setIsLoading] = useState(false);
 
   const { isAuthenticated } = useAppSelector((state) => state.userReducer);
 
   useEffect(() => {
     const verifyIsRemember = async () => {
-      const { token, refreshToken } = await checkIsAuthenticated();
+      try {
+        setIsLoading(true);
+        const { token, refreshToken } = await checkIsAuthenticated();
 
-      if (token) {
-        dispatch(updateIsAuthenticated());
-      }
+        if (token) {
+          dispatch(updateIsAuthenticated());
+        }
 
-      if (!token && refreshToken) {
-        await dispatch(userRefreshToken() as any);
-      }
+        if (!token && refreshToken) {
+          await dispatch(userRefreshToken() as any);
+        }
 
-      if (!token && !refreshToken) {
-        push("/sign-in");
+        if (!token && !refreshToken) {
+          push("/sign-in");
+        }
+      } finally {
+        setIsLoading(false);
       }
     };
 
@@ -44,7 +51,7 @@ export const AuthenticationGuard = ({ children }: PrivateRouteProps) => {
 
   return (
     <>
-      {!isAuthenticated && null}
+      {isLoading && <LoadingComponent />}
       {isAuthenticated && children}
     </>
   );
